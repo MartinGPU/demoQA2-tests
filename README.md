dfcf6200db4b3434b9747aa09691b27a4ea34741
MartinGPU <maratussr@gmail.com>
2022-11-14 19:37:42 +0400

    refactor: 1) added files readme and gitignore 2) rename directories and classes 3) beautify code text

---
+++ README.md
@@ -0,0 +1,2 @@
+Проверка формы регистрации Student Registration Form https://demoqa.com/automation-practice-form
+DemoQaMyResolutionTest.java
--- src/test/java/com/marat/tests/RegistrationFormTests.java
+++ src/test/java/com/marat/tests/RegistrationFormTests.java
@@ -1,48 +1,45 @@
-package com.marat.m;
+package com.marat.tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
-public class DemoQaMyResolutionTest {
-    @Test
-    void taskResolution(){
-    //Person
-    open("https://demoqa.com/automation-practice-form");
-    $("#firstName").setValue("Crocodile");
-    $("#lastName").setValue("Dandy");
-    $("#userEmail").setValue("Dandy@mail.gru");
-    //Gender
-    $(byText("Male")).click();
-    $("#userNumber").setValue("1234567890");
+public class RegistrationFormTests {
 
-    //birthDate
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "2560x1440";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Crocodile");
+        $("#lastName").setValue("Dandy");
+        $("#userEmail").setValue("Dandy@mail.gru");
+        $(byText("Male")).click();
+        $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
-        //Month
         $(byClassName("react-datepicker__month-select")).click();
         $(byValue("1")).click();
-        //Year
         $(byClassName("react-datepicker__year-select")).click();
         $(byValue("1987")).click();
-        //Day
         $(byClassName("react-datepicker__day--010")).click();
-    //subject
         $("#subjectsInput").setValue("Math");
         $("#subjectsInput").pressEnter();
-    //hobbies
         $(byText("Music")).click();
-    //upload file
         $("#uploadPicture").uploadFromClasspath("img/photo_2022-11-10_13-45-13.jpg");
-    //form filling
         $("#currentAddress").setValue("Pushkin Street 22");
         $("#react-select-3-input").setValue("Haryana");
         $("#react-select-3-input").pressEnter();
         $("#react-select-4-input").setValue("Karnal");
         $("#react-select-4-input").pressEnter();
         $("#submit").pressEnter();
-    //modal-body checking
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(byTagName("tbody")).shouldHave(text("Crocodile Dandy"));
         $(byTagName("tbody")).shouldHave(text("Dandy@mail.gru"));
@@ -54,7 +51,6 @@ public class DemoQaMyResolutionTest {
         $(byTagName("tbody")).shouldHave(text("photo_2022-11-10_13-45-13.jpg"));
         $(byTagName("tbody")).shouldHave(text("Pushkin Street 22"));
         $(byTagName("tbody")).shouldHave(text("Haryana Karnal"));
-        // close modal-body
         $(byText("Close")).pressEnter();
     }
 }
