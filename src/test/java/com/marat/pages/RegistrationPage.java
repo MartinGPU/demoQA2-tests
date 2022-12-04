32d5112757c6ddf2087d9db63663d2644d119623
MartinGPU <80305279+MartinGPU@users.noreply.github.com>
2022-12-04 21:05:48 +0400

    Update RegistrationPage.java

--- src/test/java/com/marat/pages/RegistrationPage.java
+++ src/test/java/com/marat/pages/RegistrationPage.java
@@ -1,7 +1,6 @@
 package com.marat.pages;
 
 import com.codeborne.selenide.SelenideElement;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
